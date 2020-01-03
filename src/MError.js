6f46c369c73e662078a64257a583e2df4656205a
wahao-mac <wuhaohao1993@outlook.com>
2020-01-03 13:21:35 +0800

    增强可靠性

--- src/MError.js
+++ src/MError.js
@@ -18,38 +18,46 @@
 */
 
 // 日志实时管理- 方便排查
-const realtimeLogManager = wx.getRealtimeLogManager();
+const realtimeLogManager = wx.getRealtimeLogManager ? wx.getRealtimeLogManager() : null
 // 获取当前小程序的账户信息
-const accountInfo = wx.getAccountInfoSync();
+const accountInfo = wx.getAccountInfoSync ? wx.getAccountInfoSync() : {};
 // 获取当前系统信息
-const systemInfo = wx.getSystemInfoSync();
+const systemInfo = wx.getSystemInfoSync ? wx.getSystemInfoSync() : {};
 // 获取启动信息
-const launchOptions = wx.getLaunchOptionsSync();
+const launchOptions = wx.getLaunchOptionsSync ? wx.getLaunchOptionsSync() : {};
 // 请求地址
 
 
 module.exports = class MError {
   constructor(key = "", title) {
-    this.title = title || (accountInfo.miniProgram ? `${accountInfo.miniProgram.appId}` : `${accountInfo.plugin.appId }(${accountInfo.plugin && accountInfo.plugin.version || ""}) `) || "小程序"
-    this.key = key || ""
-    this.host = `https://push.hellyw.com/${this.key}`
-    let self = this
-    wx.onError((error) => {
-      self.log(error)
-    })
+    try {
+      this.title = title || (accountInfo.miniProgram ? `${accountInfo.miniProgram.appId}` : `${accountInfo.plugin.appId }(${accountInfo.plugin && accountInfo.plugin.version || ""}) `) || "小程序"
+      this.key = key || ""
+      this.host = `https://push.hellyw.com/${this.key}`
+      let self = this
+      wx.onError((error) => {
+        self.log(error)
+      })
+    } catch (err) {
+      console.error(err)
+    }
   }
   uploadLog(obj) {
     let self = this
-    wx.request({
-      url: self.host,
-      header: {
-        "content-typ": "application/json"
-      },
-      method: "POST",
-      data: obj
-    })
-    realtimeLogManager.error(obj)
-    console.error(obj)
+    try {
+      console.error(obj)
+      realtimeLogManager && realtimeLogManager.error(obj)
+      wx.request({
+        url: self.host,
+        header: {
+          "content-typ": "application/json"
+        },
+        method: "POST",
+        data: obj
+      })
+    } catch (err) {
+      console.error(err)
+    }
   }
   _getUserInfo() {
     return new Promise((resolve, reject) => {
@@ -84,7 +92,6 @@ module.exports = class MError {
       try {
         wx.getSetting({
           complete: async res => {
-            // 不被授权
             try {
               if (!res.authSetting || (!res.authSetting['scope.userInfo'] && !res.authSetting['scope.userLocation'])) return resolve({})
               resolve({
@@ -102,23 +109,27 @@ module.exports = class MError {
     })
   }
   log(error) {
-    let params = {
-      title: this.title,
-      content: `${typeof error !== 'object' ? error : '当前程序出现一个错误，请及时关注'} [更详细的错误参数可进入“iGot”小程序内查看]`,
-      error: typeof error === 'object' ? error : '错误日志已打印。更多细节，请通过微信后台【“开发->运维中心->实时日志”】查看',
-      extras: Object.assign({}, {
-        accountInfo: accountInfo,
-        systemInfo: systemInfo,
-        launchOptions: launchOptions,
-        storageInfo: wx.getStorageInfoSync()
+    try {
+      let params = {
+        title: this.title,
+        content: `${typeof error !== 'object' ? error : '当前程序出现一个错误，请及时关注'} [更详细的错误参数可进入“iGot”小程序内查看]`,
+        error: typeof error === 'object' ? error : '错误日志已打印。更多细节，请通过微信后台【“开发->运维中心->实时日志”】查看',
+        extras: Object.assign({}, {
+          accountInfo: accountInfo,
+          systemInfo: systemInfo,
+          launchOptions: launchOptions,
+          storageInfo: wx.getStorageInfoSync ? wx.getStorageInfoSync() : {}
+        })
+      }
+      this.getUser().then(userInfo => {
+        params.extras = Object.assign({}, params.extras, {
+          user: userInfo
+        })
+        this.uploadLog(params)
       })
+    } catch (err) {
+      console.error(err)
     }
-    this.getUser().then(userInfo => {
-      params.extras = Object.assign({}, params.extras, {
-        user: userInfo
-      })
-      this.uploadLog(params)
-    })
 
   }
 }
