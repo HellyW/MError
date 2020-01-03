fe75f61857a2b3d6b0d5fab0ba8351c9dbc9fcaa
wahao-mac <wuhaohao1993@outlook.com>
2020-01-03 12:25:15 +0800

    1.0.0

---
+++ README.md
@@ -0,0 +1,43 @@
+
+### 配置域名
+
+> 打开微信小程序后台 , 配置下方域名为合法域名
+> 
+
+```https://push.hellyw.com```
+
+![](https://mmbiz.qpic.cn/mmbiz_png/icWtmsIaSA69ibd71LIw9X2wsaNLXLAxlQOpQfJA94bpl97holPE1JkjfTO82VKic3iaBqXR0nJLmWPibebFMuK8YRQ/0?wx_fmt)
+
+ 需要借助iGot聚合推送实现错误消息的即时送达
+ 
+ ### 引入js文件
+ 
+[点击下载](./src/MError.js)
+
+**为方便接入，建议更名为`MError.js`文件**
+
+### 部署代码
+
+> 在 `app.json` 中引入该js文件 
+>
+
+```
+const MError = require('/utils/MError')
+```
+
+>在`onLaunch`内加入如下代码，`key`更换为您自己的key 
+> **建议使用临时key，如需项目组多人接收，建议使用不公开的订阅链接key。获取方式：https://support.qq.com/products/111465/faqs/58267**
+> 
+
+```
+!this.mError && ( this.mError = new MError(key,'推送标题') )
+```
+
+### 效果
+
+至此，就已经成功实现了 。   来看看效果吧！！！
+![ ](https://mmbiz.qpic.cn/mmbiz_jpg/icWtmsIaSA69ibd71LIw9X2wsaNLXLAxlQ3iamJwvZ0ckUUEM6n3wfSF86qwNHwAXoKibUMSBXDIt8ycYC7xrQ46hA/0?wx_fmt=jpeg)
+
+手机端成功接收。
+
+
---
+++ src/MError.js
@@ -0,0 +1,124 @@
+/* 
+
+  v1.0.0
+
+  * 推送提醒：
+
+    服务依赖iGot小程序， 微信搜索“iGot”体验
+
+    key的获取方式请参照：https://support.qq.com/products/111465/faqs/58267
+
+    iGot桌面客户端 [windows && mac] 下载 ： https://github.com/wahao/Electron-iGot
+
+  * 微信实时日志：
+
+    开发者可从小程序管理后台“开发->运维中心->实时日志”进入日志查询页面，查看开发者打印的日志信息。
+
+
+*/
+
+// 日志实时管理- 方便排查
+const realtimeLogManager = wx.getRealtimeLogManager();
+// 获取当前小程序的账户信息
+const accountInfo = wx.getAccountInfoSync();
+// 获取当前系统信息
+const systemInfo = wx.getSystemInfoSync();
+// 获取启动信息
+const launchOptions = wx.getLaunchOptionsSync();
+// 请求地址
+
+
+module.exports = class MError {
+  constructor(key = "", title) {
+    this.title = title || (accountInfo.miniProgram ? `${accountInfo.miniProgram.appId}` : `${accountInfo.plugin.appId }(${accountInfo.plugin && accountInfo.plugin.version || ""}) `) || "小程序"
+    this.key = key || ""
+    this.host = `https://push.hellyw.com/${this.key}`
+    let self = this
+    wx.onError((error) => {
+      self.log(error)
+    })
+  }
+  uploadLog(obj) {
+    let self = this
+    wx.request({
+      url: self.host,
+      header: {
+        "content-typ": "application/json"
+      },
+      method: "POST",
+      data: obj
+    })
+    realtimeLogManager.error(obj)
+    console.error(obj)
+  }
+  _getUserInfo() {
+    return new Promise((resolve, reject) => {
+      try {
+        wx.getUserInfo({
+          complete: (res) => {
+            resolve(res.userInfo || {})
+          }
+        })
+      } catch (err) {
+        resolve({})
+      }
+    })
+  }
+  _getLocation() {
+    return new Promise((resolve, reject) => {
+      try {
+        wx.getLocation({
+          complete: (res) => {
+            resolve(res || {})
+          }
+        })
+      } catch (err) {
+        resolve({})
+      }
+    })
+  }
+  getUser() {
+    // 读取设置 - 已被允许的权限 尽可能获取
+    let self = this
+    return new Promise((resolve, reject) => {
+      try {
+        wx.getSetting({
+          complete: async res => {
+            // 不被授权
+            try {
+              if (!res.authSetting || (!res.authSetting['scope.userInfo'] && !res.authSetting['scope.userLocation'])) return resolve({})
+              resolve({
+                userInfo: res.authSetting['scope.userInfo'] ? await self._getUserInfo() : {},
+                location: res.authSetting['scope.userLocation'] ? await self._getLocation() : {},
+              })
+            } catch (err) {
+              resolve({})
+            }
+          }
+        })
+      } catch (err) {
+        resolve({})
+      }
+    })
+  }
+  log(error) {
+    let params = {
+      title: this.title,
+      content: `${typeof error !== 'object' ? error : '当前程序出现一个错误，请及时关注'} [更详细的错误参数可进入“iGot”小程序内查看]`,
+      error: typeof error === 'object' ? error : '错误日志已打印。更多细节，请通过微信后台【“开发->运维中心->实时日志”】查看',
+      extras: Object.assign({}, {
+        accountInfo: accountInfo,
+        systemInfo: systemInfo,
+        launchOptions: launchOptions,
+        storageInfo: wx.getStorageInfoSync()
+      })
+    }
+    this.getUser().then(userInfo => {
+      params.extras = Object.assign({}, params.extras, {
+        user: userInfo
+      })
+      this.uploadLog(params)
+    })
+
+  }
+}
